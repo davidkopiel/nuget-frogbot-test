2b7d3f81e99afcd5544c91f35fb63c85c118ee41
David Kopiel <davidko@jfrog.com>
2025-06-29 11:05:37 +0300

    feat: Add a comment to test PR scan

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-// Adding a comment to trigger a PR scansd
+// Adding a comment to trigger a PR scansds
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
